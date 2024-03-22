ae9f0c1f2c8f5438a5f73c9705c216125d9e652f
Faqihach <faqihach1@gmail.com>
2024-03-23 02:41:51 +0700

    menambahkan pertemuan 4

---
+++ pertemuan4/date.php
@@ -0,0 +1,36 @@
+<?php
+// FUNCTION, 1. Built in, 2. User-defined Function
+// function ada banyak:
+
+// String:
+// strlen()
+// strcmp()
+// explode()
+// htmlspecialchars()
+
+// Utility:
+// vardump_() mencetak isi dari sebuah variabel
+// isset() untuk cek apakah nama var sudah digunakan
+// empty() untuk cek apakah nama var msh kosong
+// die() untuk berhentikan program kita
+// sleep() berhentikan sementara
+// DLL
+
+// date, untuk menampilkan hari, bulan, tanggal
+// echo date("l");
+
+// TIME , untuk menampilkan waktu
+// UNIX timestamp / EPOCH time
+// waktu yang menunjukkan sejak tgl 1 januari 1970
+// echo time();
+
+// hitung 100 hari kedepan 
+// echo date("l, d-M-Y", time()+60*60*24*100);
+
+// mktime(0,0,0,0,0,0),membuat sendiri detik
+// jam, menit, detik, bulan, tanggal, tahun
+// echo date( "l", mktime(0,0,0,5,22,2004));
+
+// strtotime, tanggal ke detik kebalikan mktime
+// echo date("l", strtotime("12 May 2004"));
+?>
--- pertemuan4/function.php
+++ pertemuan4/function.php
@@ -1,5 +1,21 @@
-<?php 
-// FUNCTION 
+<?php
+// User-Defined Function
 
+function salam($waktu = "Datang", $nama = "Admin") {
+    return "Selamat $waktu, $nama";
+    
+}
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Function</title>
+</head>
+<body>
+    <h1><?php echo salam("Dinihari", "Ahmad") ?></h1>
+</body>
+</html>
